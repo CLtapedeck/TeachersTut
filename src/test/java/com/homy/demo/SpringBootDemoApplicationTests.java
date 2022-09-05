f4b9dc798526de7fe2685c9e79ea351464d8330b
Cal Lamshed <s3706239@student.rmit.edu.au>
2022-09-05 12:36:41 +1000

    Adding test for teacherNumber

--- src/test/java/com/homy/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/homy/demo/SpringBootDemoApplicationTests.java
@@ -1,6 +1,10 @@
 package com.homy.demo;
 
+import com.homy.rest.controller.TeacherController;
+import com.homy.rest.dao.TeacherDAO;
+import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,6 +16,11 @@ public class SpringBootDemoApplicationTests {
 	@Test
 	public void contextLoads() {
 
+	}
+	@Test
+	public void checkTeacherNumber() {
+		TeacherDAO testDAO = new TeacherDAO();
+		Assert.assertEquals((testDAO.getAllTeachers()).getTeacherList().size(),3);
 	}
 
 }
